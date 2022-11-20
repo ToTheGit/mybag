0bce71ae726633013587eed6c94555933013a423
NAMWONSHIK <nws717@naver.com>
2022-11-21 00:17:06 +0900

    initialize NAMWONSHIK-server

--- src/main/java/kr/ac/gachon/shop/controller/MainController.java
+++ src/main/java/kr/ac/gachon/shop/controller/MainController.java
@@ -1,9 +1,7 @@
 package kr.ac.gachon.shop.controller;
 
-import ch.qos.logback.classic.Logger;
 import groovy.util.logging.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -12,9 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String index(Model model){
-        Logger log = null;
-        log.info("index 컨트롤러");
+    public String index(){
+
         return "index";
     }
 }
