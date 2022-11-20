6aadd14d34997aeb679196728f741a89c4debfbf
NAMWONSHIK <nws717@naver.com>
2022-11-20 19:38:41 +0900

    initialize NAMWONSHIK-server

--- src/main/java/kr/ac/gachon/shop/repository/MemberRepository.java
+++ src/main/java/kr/ac/gachon/shop/repository/MemberRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findByEmail(String email);
-    //멤버 repository
+    //멤버 repository라고라고라고라고라고라고라고
 }
